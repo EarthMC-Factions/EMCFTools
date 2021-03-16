3c6b66d00ef425a544d5dfb5cbafe7786a72bbcf
Warrior <50800980+Warriorrrr@users.noreply.github.com>
2021-03-16 21:43:10 +0100

    Replace chests with bedrock when clicked

--- src/main/java/dev/warriorrr/emcftools/listeners/BlockListener.java
+++ src/main/java/dev/warriorrr/emcftools/listeners/BlockListener.java
@@ -3,12 +3,14 @@ package dev.warriorrr.emcftools.listeners;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
+import org.bukkit.Tag;
 import org.bukkit.block.BlockState;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.world.PortalCreateEvent;
 
 import net.md_5.bungee.api.ChatColor;
@@ -33,8 +35,11 @@ public class BlockListener implements Listener {
     @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
     public void onBlockPlace(BlockPlaceEvent event) {
         if (event.getBlock().getY() == 0 && event.getPlayer().getGameMode() != GameMode.CREATIVE) {
-            event.getPlayer().sendMessage(prefix + ChatColor.RED + " You cannot place a block at this location.");
-            event.getBlock().setType(Material.BEDROCK);
+            event.getPlayer().sendMessage(prefix + ChatColor.RED + " You cannot place this block at this location.");
+            if (!isBlockEntity(event.getBlockPlaced().getType())) // Replacing block entities seems to cause console spam.
+                event.getBlock().setType(Material.BEDROCK);
+            else
+                event.setCancelled(true);
         }
     }
 
@@ -44,9 +49,63 @@ public class BlockListener implements Listener {
     @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
     public void onBlockBreak(BlockBreakEvent event) {
         if (event.getBlock().getY() == 0 && event.getPlayer().getGameMode() != GameMode.CREATIVE) {
-            event.getPlayer().sendMessage(prefix + ChatColor.RED + " You cannot break a block at this location.");
+            event.getPlayer().sendMessage(prefix + ChatColor.RED + " You cannot break this block at this location.");
             event.setCancelled(true);
             event.getBlock().setType(Material.BEDROCK);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
+    public void onOpenChest(PlayerInteractEvent event) {
+        if ((event.getClickedBlock().getType() == Material.CHEST || event.getClickedBlock().getType() == Material.TRAPPED_CHEST) && event.getClickedBlock().getY() == 0) {
+            event.getPlayer().sendMessage(prefix + ChatColor.RED + " You cannot interact with this block at this location.");
+            event.getClickedBlock().setType(Material.BEDROCK);
+        }
+    }
+
+    private boolean isBlockEntity(Material material) {
+        if (Tag.SIGNS.isTagged(material) || Tag.WALL_SIGNS.isTagged(material) || Tag.BANNERS.isTagged(material) || Tag.SHULKER_BOXES.isTagged(material))
+            return true;
+        switch(material) {
+            case BEEHIVE:
+            case BEE_NEST:
+            case CHEST:
+            case TRAPPED_CHEST:
+            case DISPENSER:
+            case FURNACE:
+            case BREWING_STAND:
+            case HOPPER:
+            case DROPPER:
+            case BARREL:
+            case SMOKER:
+            case BLAST_FURNACE:
+            case CAMPFIRE:
+            case SOUL_CAMPFIRE:
+            case LECTERN:
+            case BEACON:
+            case SPAWNER:
+            case JUKEBOX:
+            case ENCHANTING_TABLE:
+            case END_PORTAL:
+            case ENDER_CHEST:
+            case PLAYER_HEAD:
+            case PLAYER_WALL_HEAD:
+            case CREEPER_HEAD:
+            case CREEPER_WALL_HEAD:
+            case ZOMBIE_HEAD:
+            case ZOMBIE_WALL_HEAD:
+            case SKELETON_SKULL:
+            case SKELETON_WALL_SKULL:
+            case WITHER_SKELETON_SKULL:
+            case WITHER_SKELETON_WALL_SKULL:
+            case DRAGON_HEAD:
+            case DRAGON_WALL_HEAD:
+            case CAULDRON:
+            case CONDUIT:
+            case BELL:
+                return true;
+            default:
+                return false;
         }
     }
 }
